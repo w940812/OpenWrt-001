511ae751d7f1323d41d6797c80f8a7fe3ad8271e
w940812 <124517478+w940812@users.noreply.github.com>
2026-03-29 23:46:14 +0800

    fix: use https:// for all feed URLs to avoid git protocol issues in GitHub Actions

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add feed sources
+# Add feed sources (use https:// for GitHub Actions compatibility)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore-openwrt.git' >>feeds.conf.default
