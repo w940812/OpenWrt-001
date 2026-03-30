a73d49e76730af8550cd4bd2e8bec69621150a17
w940812 <124517478+w940812@users.noreply.github.com>
2026-03-30 13:37:27 +0800

    fix: 禁用 ksmbd 内核模块 (与内核 6.12 API 不兼容)

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,9 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Disable ksmbd (incompatible with kernel 6.12)
+# ksmbd 3.5.4 has API incompatibility with newer kernels (struct file_lock changes)
+sed -i 's/CONFIG_PACKAGE_kmod-ksmbd=y/# CONFIG_PACKAGE_kmod-ksmbd is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_ksmbd-tools=y/# CONFIG_PACKAGE_ksmbd-tools is not set/g' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-ksmbd=y/# CONFIG_PACKAGE_luci-app-ksmbd is not set/g' .config
