194983d347b8c759532c62ee29bf48baa8b2a17c
w940812 <124517478+w940812@users.noreply.github.com>
2026-03-30 10:21:40 +0800

    fix: 更新过期的 Feed 源地址 (passwall/istore/docker 404错误)

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,6 @@
 
 # Add feed sources (do NOT use sed to uncomment, feeds.conf.default already has commented helloworld)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore-openwrt.git' >>feeds.conf.default
-echo 'src-git docker https://github.com/SuLingGGV/openwrt-docker.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore' >>feeds.conf.default
+# docker feed 已废弃移除，如需 Docker 支持请在 make menuconfig 中启用 CONFIG_PACKAGE_docker 等选项
